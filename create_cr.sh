318484c9eff5259913f7fe5301deb9e29e7cb81c
Kapil Shukla <kgshukla@yahoo.com>
2020-09-15 21:04:02 +0800

    first commit

--- create_cr.sh
+++ create_cr.sh
@@ -1,3 +1,4 @@
+oc project openshift-metering
 oc create -f rds-custom-node-capacity-cpu-cores.yaml 
 oc create -f rq-custom-node-cpu-capacity-raw.yaml
 oc create -f rds-custom-node-cpu-capacity-raw.yaml
--- delete_cr.sh
+++ delete_cr.sh
@@ -1,3 +1,4 @@
+oc project openshift-metering
 oc delete rq/custom-role-node-cpu-capacity-labels
 oc delete reportdatasource/custom-role-node-cpu-capacity-raw
 oc delete rq/custom-role-node-cpu-capacity-raw
