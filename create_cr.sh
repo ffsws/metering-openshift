a223dea79035a3e1a21205b9b63790fff524b518
Kapil Shukla <kgshukla@yahoo.com>
2020-09-15 20:50:49 +0800

    first commit

---
+++ create_cr.sh
@@ -0,0 +1,4 @@
+oc create -f rds-custom-node-capacity-cpu-cores.yaml 
+oc create -f rq-custom-node-cpu-capacity-raw.yaml
+oc create -f rds-custom-node-cpu-capacity-raw.yaml
+oc create -f rq-custom-node-cpu-capacity-with-cpus-labels.yaml
---
+++ delete_cr.sh
@@ -0,0 +1,4 @@
+oc delete rq/custom-role-node-cpu-capacity-labels
+oc delete reportdatasource/custom-role-node-cpu-capacity-raw
+oc delete rq/custom-role-node-cpu-capacity-raw
+oc  delete reportdatasource/custom-role-node-capacity-cpu-cores
